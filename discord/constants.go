01658ba67448ee62e784282432e1bb4f5f576333
brnlee <Brianleenyc@gmail.com>
2020-05-08 17:48:48 -0400

    Anonymized constants

---
+++ discord/constants.go
@@ -0,0 +1,9 @@
+package discord
+
+const (
+	ChannelID   = "Discord channel id"
+	MasterToken = "Token of the Discord master bot"
+	BotToken    = "Token of the Discord slave bot"
+	Timeout     = 15
+	Secret      = "Encryption Secret"
+)
